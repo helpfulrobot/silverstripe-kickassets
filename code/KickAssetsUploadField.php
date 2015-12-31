6ee5baa6c60c95c32ce5b5ed427854681049f5fe
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:47:25 +1300

    Converted to PSR-2

--- code/KickAssetsUploadField.php
+++ code/KickAssetsUploadField.php
@@ -1,15 +1,17 @@
 <?php
 
-class KickAssetsUploadField extends DataExtension {
+class KickAssetsUploadField extends DataExtension
+{
 
-	private static $allowed_actions = array (
-		'folderid'
-	);
+    private static $allowed_actions = array(
+        'folderid'
+    );
 
 
-	public function folderid(SS_HTTPRequest $r) {
-		return new SS_HTTPResponse(
-			Folder::find_or_make($this->owner->getFolderName())->ID
-		);
-	}
+    public function folderid(SS_HTTPRequest $r)
+    {
+        return new SS_HTTPResponse(
+            Folder::find_or_make($this->owner->getFolderName())->ID
+        );
+    }
 }
